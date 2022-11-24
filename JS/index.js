c20d647d13e6c41af05d8088032cb0872e247913
Navier Cracco <naviercracco@gmail.com>
2022-11-24 18:19:35 -0300

    primer commit

---
+++ JS/index.js
@@ -0,0 +1,65 @@
+const productos = [
+    {
+        id: "abrigo-01",
+        titulo: "abrigo 01",
+        imagen: "./img/abrigos/abrigo-01.jpg",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos",
+        },
+        precio: 5000,
+    },
+    {
+        id: "abrigo-02",
+        titulo: "abrigo 02",
+        imagen: "./img/abrigos/abrigo-02.jpg",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos",
+        },
+        precio: 5000,
+    },
+    {
+        id: "abrigo-03",
+        titulo: "abrigo 03",
+        imagen: "./img/abrigos/abrigo-03.jpg",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos",
+        },
+        precio: 5000,
+    },
+    {
+        id: "abrigo-04",
+        titulo: "abrigo 04",
+        imagen: "./img/abrigos/abrigo-04.jpg",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos",
+        },
+        precio: 5000,
+    },
+    
+]
+
+
+
+
+// Swal.fire({
+//     title: 'Email',
+//     input: 'text',
+//     input: 'text',
+//     inputAttributes: {
+//       autocapitalize: 'off'
+//     },
+//     showCancelButton: true,
+//     confirmButtonText: 'Look up',
+//     showLoaderOnConfirm: true,
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: `${result.value}`,
+//         imageUrl: result.value
+//       })
+//     }
+//   })
